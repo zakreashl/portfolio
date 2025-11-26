867cbfa371a77db36ca55f901c694e4c2796bc6f
Zakrea <139512367+zakreashl@users.noreply.github.com>
2025-11-25 22:39:24 -0600

    Added projects

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -5,7 +5,10 @@ import ProjectSquare from "../ProjectSquare";
 import Footer from "../Footer";
 
 import react from "./../assets/react.svg";
-import luminata from "./../assets/luminata.png"
+import luminata from "./../assets/luminata.png";
+import heap from "./../assets/heap.jpg"
+import stack from "./../assets/stack.png"
+import cShell from "./../assets/cShell.png"
 
 
 function Projects() {
@@ -26,6 +29,30 @@ function Projects() {
             alt={"luminata thumbnail"}
             header={"Games"}
             description={"All of the Unity games I have made"}
+            />
+
+            <ProjectSquare
+            link={"https://github.com/zakreashl/My_malloc"}
+            bg={heap}
+            alt={"Heap icon"}
+            header={"Heap"}
+            description={"A mini heap I made with malloc and free functions"}
+            />
+
+            <ProjectSquare
+            link={"https://github.com/zakreashl/Toy-Stack-in-C-and-ASM"}
+            bg={stack}
+            alt={"Stack icon"}
+            header={"Stack"}
+            description={"A mini stack I made with C and ASM with push and pop functions"}
+            />
+
+            <ProjectSquare
+            link={"https://github.com/zakreashl/cShell"}
+            bg={cShell}
+            alt={"cShell"}
+            header={"cShell"}
+            description={"A custom shell I made in c"}
             />
         </ProjectSquareContainer>
 
